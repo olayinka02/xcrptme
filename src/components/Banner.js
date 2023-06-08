62996737021c9476bce045f7500f45bdeca7632c
Olayinka <64751867+olayinka02@users.noreply.github.com>
2023-06-08 06:14:21 +0100

    xcrptme

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -17,22 +17,8 @@ function Banner() {
      
     
         <Container fluid >
-          <Row >
-            <Col md={8} lg={8} sm={12}>
-            <p style={{color:'white'}}>a:hello will you be my friend. Note that the development build is not optimized.
-                To create a production build, use npm run build.  Note that the development build is not optimized.
-                To create a production build, use npm run build.
-              </p>
-            </Col>
-
-            
-            <Col md={4} lg={4} sm={12}>
-             <p style={{color:'white'}}>b:hello will you be my friend. Note that the development build is not optimized.
-                To create a production build, use npm run build.  Note that the development build is not optimized.
-                To create a production build, use npm run build.
-              </p> 
-            </Col>
-            </Row>
+          
+          <p>hello ma</p>
           
 
 
